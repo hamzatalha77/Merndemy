4f994995063bd3605bd749ff3e212c9bcb7b0ef5
hamzatalha77 <hamza_ta7@hotmail.com>
2024-02-05 12:24:15 +0100

    COMMIT

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose'
 
 mongoose.set('strictQuery', false)
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {})
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
+
 export default connectDB
