a3ba2285e8f2995006973071116e74c589d2c866
hamzatalha77 <hamza_ta7@hotmail.com>
2024-03-13 19:31:26 +0000

    commit

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,12 +6,14 @@ import {
   updateOrderToPaid,
   getMyOrders,
   getOrders,
-  updateOrderToDelivered
+  updateOrderToDelivered,
+  deleteOrder
 } from '../controllers/orderController.js'
 import { protect, admin } from '../middleware/authMiddleWare.js'
 
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
+router.route('/myorders/:id').delete(protect, deleteOrder)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
